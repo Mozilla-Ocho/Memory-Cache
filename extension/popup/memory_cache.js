bae6543521edf4e8519ffdc9bdd754433a3c1c20
bharattech <bharattech@protonmail.com>
2024-01-17 19:29:13 +0530

    Issue-10-Make-readable-file-name-with-suggested-changes

--- extension/popup/memory_cache.js
+++ extension/popup/memory_cache.js
@@ -4,18 +4,13 @@ const DOWNLOAD_SUBDIRECTORY = "MemoryCache";
 Generate a file name based on date and time
 */
 
-function getTabTitle() {}
-
 async function generateFileName(ext) {
-  var subfileName =
-    new Date().toISOString().concat(0, 19).replaceAll(":", ".") + "." + ext;
-
+  let subfileName = `${new Date().toISOString().replaceAll(":", ".")}.${ext}`;
   return browser.tabs
     .query({ active: true })
     .then((tabs) => {
       if (tabs && tabs[0] && tabs[0].title) {
-        console.log(tabs[0].title);
-        return tabs[0].title.replaceAll(" ", "-") + "-" + subfileName;
+        return `${tabs[0].title.replaceAll(" ", "-")}-${subfileName}`;
       } else {
         return subfileName;
       }
@@ -27,7 +22,7 @@ async function generateFileName(ext) {
 
 async function savePDF() {
   try {
-    var fileName = await generateFileName("pdf");
+    let fileName = await generateFileName("pdf");
     await browser.tabs.saveAsPDF({
       toFileName: `/${DOWNLOAD_SUBDIRECTORY}/PAGE-${fileName}`,
       silentMode: true, // silentMode requires a custom build of Firefox
@@ -54,8 +49,8 @@ function send(message) {
 }
 
 async function saveHtml() {
-  var fileName = await generateFileName("html");
   const text = await send({ action: "getPageText" });
+  let fileName = await generateFileName("html");
   fileName = `${DOWNLOAD_SUBDIRECTORY}/PAGE-${fileName}`;
   const file = new File([text], fileName, { type: "text/plain" });
   const url = URL.createObjectURL(file);
@@ -63,8 +58,8 @@ async function saveHtml() {
 }
 
 async function saveNote() {
-  var fileName = await generateFileName("txt");
   const text = document.querySelector("#text-note").value;
+  let fileName = await generateFileName("txt");
   fileName = `${DOWNLOAD_SUBDIRECTORY}/NOTE-${fileName}`;
   const file = new File([text], fileName, { type: "text/plain" });
   const url = URL.createObjectURL(file);
