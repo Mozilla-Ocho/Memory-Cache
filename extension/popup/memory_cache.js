6c37339e0eb13a3679d51b5d0c087376b088c51c
katetaylormoz <kataylor@mozilla.com>
2024-03-08 16:10:49 -0500

    extension markdown state

--- extension/popup/memory_cache.js
+++ extension/popup/memory_cache.js
@@ -5,47 +5,23 @@ const DOWNLOAD_SUBDIRECTORY = "MemoryCache";
 /*
 Generate a file name based on date and time
 */
-
-async function generateFileName(ext) {
-  let subfileName = `${new Date().toISOString().replaceAll(":", ".")}.${ext}`;
-  return browser.tabs
-    .query({ active: true })
-    .then((tabs) => {
-      if (tabs && tabs[0] && tabs[0].title) {
-        return `${tabs[0].title.replaceAll(" ", "-")}-${subfileName}`;
-      } else {
-        return subfileName;
-      }
-    })
-    .then ((fileName)=> {
-      return fileName.replaceAll(":","")
-                     .replaceAll("!","")
-                      .replaceAll("?","")
-                      .replaceAll("/","")
-                      .replaceAll("\\","")
-                      .replaceAll("*","")
-                      .replaceAll("|","")
-                      .replaceAll(" ","")
-                      .replaceAll("<","")
-                      .replaceAll(">","");
-    })
-    .catch((error) => {
-      reject(`Error querying tabs: ${error}`);
-    });
+function generateFileName(ext) {
+  return (
+    new Date().toISOString().concat(0, 19).replaceAll(":", ".") + "." + ext
+  );
 }
 
 async function savePDF() {
   try {
-    let fileName = await generateFileName("pdf");
     await browser.tabs.saveAsPDF({
-      toFileName: `/${DOWNLOAD_SUBDIRECTORY}/PAGE-${fileName}`,
+      toFileName: `${DOWNLOAD_SUBDIRECTORY}/PAGE${generateFileName("pdf")}`,
       silentMode: true, // silentMode requires a custom build of Firefox
     });
   } catch (_e) {
     // Fallback to non-silent mode.
     await browser.tabs.saveAsPDF({
       // Omit the DOWNLOAD_SUBDIRECTORY prefix because saveAsPDF will not respect it.
-      toFileName: `PAGE-${await generateFileName("pdf")}`,
+      toFileName: `PAGE${generateFileName("pdf")}`,
     });
   }
 }
@@ -64,21 +40,18 @@ function send(message) {
 
 async function saveHtml() {
   const text = await send({ action: "getPageText" });
-  let fileName = await generateFileName("html");
-  fileName = `${DOWNLOAD_SUBDIRECTORY}/PAGE-${fileName}`;
-  console.log(fileName);
-  const file = new File([text], fileName, { type: "text/plain" });
+  const filename = `${DOWNLOAD_SUBDIRECTORY}/PAGE${generateFileName("html")}`;
+  const file = new File([text], filename, { type: "text/plain" });
   const url = URL.createObjectURL(file);
-  browser.downloads.download({ url, filename: fileName, saveAs: false });
+  browser.downloads.download({ url, filename, saveAs: false });
 }
 
-async function saveNote() {
+function saveNote() {
   const text = document.querySelector("#text-note").value;
-  let fileName = await generateFileName("txt");
-  fileName = `${DOWNLOAD_SUBDIRECTORY}/NOTE-${fileName}`;
-  const file = new File([text], fileName, { type: "text/plain" });
+  const filename = `${DOWNLOAD_SUBDIRECTORY}/NOTE${generateFileName("md")}`;
+  const file = new File([text], filename, { type: "text/plain" });
   const url = URL.createObjectURL(file);
-  browser.downloads.download({ url, filename: fileName, saveAs: false });
+  browser.downloads.download({ url, filename, saveAs: false });
 
   document.querySelector("#text-note").value = "";
   browser.storage.local.set({ noteDraft: "" });
